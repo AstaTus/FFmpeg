dfe1ee4ff6ac3fd8c744842516b7f6674f7ac4d2
老干部 <chenjunqi@qiniu.com>
2022-01-06 12:00:58 +0800

    when video is truely end then return eof state rather than try try again and again

--- libavformat/aviobuf.c
+++ libavformat/aviobuf.c
@@ -598,7 +598,7 @@ static void fill_buffer(AVIOContext *s)
            be done without rereading data */
         s->eof_reached = 1;
     } else if (len < 0) {
-        s->eof_reached = 1;
+//        s->eof_reached = 1;
         s->error= len;
     } else {
         s->pos += len;
@@ -672,11 +672,13 @@ int avio_read(AVIOContext *s, unsigned char *buf, int size)
                     be done without rereading data */
                     s->eof_reached = 1;
                     break;
-                } else if (len < 0) {
-                    s->eof_reached = 1;
+                }
+                else if (len < 0) {
+//                    s->eof_reached = 1;
                     s->error= len;
                     break;
-                } else {
+                }
+                else {
                     s->pos += len;
                     ffiocontext(s)->bytes_read += len;
                     s->bytes_read = ffiocontext(s)->bytes_read;
