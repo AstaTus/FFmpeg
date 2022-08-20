205aa105b3087a47e9fb261e5c4955f21d9bef40
老干部 <chenjunqi@qiniu.com>
2022-08-20 18:20:40 +0800

    if videotoolbox return success osstatus but the decode frame is null return AVERROR_INVALIDDATA instead AVERROR_UNKNOWN

--- libavcodec/videotoolbox.c
+++ libavcodec/videotoolbox.c
@@ -1028,8 +1028,8 @@ int ff_videotoolbox_common_end_frame(AVCodecContext *avctx, AVFrame *frame)
     }
 
     if (!vtctx->frame) {
-        vtctx->reconfig_needed = true;
-        return AVERROR_UNKNOWN;
+//        vtctx->reconfig_needed = true;
+        return AVERROR_INVALIDDATA;
     }
 
     return videotoolbox_buffer_create(avctx, frame);
